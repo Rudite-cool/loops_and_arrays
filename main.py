90757d24a49427b44887ccaf373d6ec9a942cea3
Rudite-cool <rudulis@gmail.com>
2025-06-16 13:35:27 +0300

    Updates on list

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ for i in range(10):
 nums=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(nums)
 
-------------------------------------------
+# ------------------------------------------
 
+plants=["rose", "ivy", "lily", "daisy", "violet", "poppy", "corn", "moss", "lotus", "cactus"]
+print(plants)
+
+for plant in plants:
+    print(plant)
+
+# -----------------
