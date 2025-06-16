ed189c3da605ed7967f38456ee3d8f4a88289bd1
Rudite-cool <rudulis@gmail.com>
2025-06-16 15:41:31 +0300

    Updates on list1

--- main.py
+++ main.py
@@ -79,5 +79,34 @@ print("middle_words:", count)
 
 numbers = [random.randint(0, 300) for _ in range(300)]
 
+output = []
+for num in numbers:
+    if num > 275:
+        output.append(f"[{num}]")
+    else:
+        output.append(str(num))
+print(output)
+count = sum(1 for num in numbers if num > 150)
+print("amount of numbers", count)
+
+# -------------------------------------------------------
+
+first = True
+for num in range(1, 3001):
+    if num % 77 == 0:
+        if not first:
+            print(", ", end="")
+        print(num, end="")
+        first = False
+print()
+# ---------------------------------------------------------
+
+print(", ".join(str(num) for num in range(1, 3001) if num % 77 == 0))
+
+# -------------------------------------------------------
+
+
+
+
 
 
