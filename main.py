752a6661adcf49033963e5b8374c8de9ffe15701
Rudite-cool <rudulis@gmail.com>
2025-06-16 14:46:06 +0300

    Updates on list1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 
 for i in range(10):
     print("labas")
@@ -13,4 +14,70 @@ print(plants)
 for plant in plants:
     print(plant)
 
-# -----------------
+ # ------------------------------
+
+plants=["rose", "ivy", "lily", "daisy", "violet", "poppy", "corn", "moss", "lotus", "cactus"]
+print(plants)
+for plant in reversed(plants):
+    print(plant)
+
+ # -----------------------------------------
+
+for number in range(10, 51, 2):
+    print(number)
+
+# -------------------------------
+for number in range(10, 51):
+    if i % 2 == 0:
+        continue
+        print(i)
+# --------------------------------------
+
+for number in range(10, 51, 2):
+    if number % 10 == 0:
+        continue
+    print(number)
+
+ # ----------------------------------
+count=0
+for i in range(0, 21):
+    if i % 2 == 0:
+        count += 1
+        print(count)
+
+# ------------------------------
+plants=["rose", "ivy", "lily", "daisy", "violet", "poppy", "corn", "moss", "lotus", "cactus"]
+print(plants)
+
+short_count=0
+long_count=0
+
+for plant in plants:
+    length=len(plant)
+    if length >= 7:
+        long_count +=1
+    if length <= 5:
+        short_count +=1
+print("shorter_words", short_count)
+print("longer_words", long_count)
+
+# -----------------------------------------------
+
+plants=["rose", "ivy", "lily", "daisy", "violet", "poppy", "corn", "moss", "lotus", "cactus"]
+print(plants)
+
+count=0
+
+for plant in plants:
+    length = len(plant)
+    if length > 5 and length < 10:
+        count += 1
+print("middle_words:", count)
+
+# ---------------------------------
+
+
+numbers = [random.randint(0, 300) for _ in range(300)]
+
+
+
